5c6323c774fadeaefc7989eb6d1276fa2eb1b865
forza111 <nikitos.ionkin@yandex.ru>
2021-01-27 17:34:48 +0300

    работа со вьюхами

--- API/polls/urls.py
+++ API/polls/urls.py
@@ -5,6 +5,7 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:polls_id>/', views.detail, name='detail'),
+    path('<int:polls_id>/', views.DetailView.as_view(), name='detail'),
     path('<int:polls_id>/info/<int:question_id>', views.detail_question, name='detail_question'),
+    path('<int:polls_id>/info/<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- API/polls/views.py
+++ API/polls/views.py
@@ -12,10 +12,17 @@ def index(request):
     context = {'latest_polls_list':latest_polls_list}
     return render(request, 'polls/index.html', context)
 
+class DetailView(generic.DetailView):
+    model = Polls
+    template_name = 'polls/detail.html'
+    def get_queryset(self, polls_id):
+        return Polls.objects.get(pk = polls_id)
 
-def detail(request, polls_id):
+
+
+'''def detail(request, polls_id):
     polls = get_object_or_404(Polls, pk=polls_id)
-    return render(request, 'polls/detail.html', {'polls': polls})
+    return render(request, 'polls/detail.html', {'polls': polls})'''
 
 
 def detail_question(request, polls_id, question_id):
@@ -23,8 +30,9 @@ def detail_question(request, polls_id, question_id):
     question = get_object_or_404(Question, polls = polls, pk = question_id)
     return render(request, 'polls/detail_question.html', {'polls': polls, 'question' : question})
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
+def vote(request, polls_id, question_id):
+    polls = get_object_or_404(Polls, pk=polls_id)
+    question = get_object_or_404(Question, polls = polls, pk = question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
 
